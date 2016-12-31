d39c130406550e4014602062c4ac3dbac9203740
Tucker Strausbaugh <strausbaugh.tucker@gmail.com>
2016-12-31 15:30:53 -0500

    load vehicles from json file, made the collision detection be within the length of a mid-size sedan, and output the 2 vehicles that collided.

--- main.cpp
+++ main.cpp
@@ -6,36 +6,55 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 #include "Vehicle.h"
+#include "json.hpp"
 
 using namespace std;
+using json = nlohmann::json;
 
 int main()
 {
     vector<Vehicle*> vehicles;
 
-    vehicles.push_back(new Vehicle(60.0, 0.0));
-    vehicles.push_back(new Vehicle(55.0, 0.05));
-    vehicles.push_back(new Vehicle(50.0, 0.10));
+    ifstream file("Vehicles.json");
+    json config;
+    file >> config;
 
-    for (auto& vehicle : vehicles)
+    for (auto& vehicle: config["vehicles"])
+        vehicles.push_back(new Vehicle(vehicle["avgVelocity"], vehicle["startingDistance"], vehicle["startingLane"], vehicle["updateRate"]));
+
+   for (auto& vehicle : vehicles)
         vehicle->drive();
 
+    unsigned int first, second;
     bool collision = false;
+    pair<unsigned int, unsigned int> collidedVehicles;
+
     while (!collision)
     {
         chrono::system_clock::time_point start = chrono::system_clock::now();
 
+        first = 1;
         for (auto& vehicle: vehicles)
         {
             cout << "location: " << vehicle->location.distance << "mi\n";
 
+            second = 1;
             for (auto& i: vehicles)
             {
-                if (vehicle != i && abs(vehicle->location.distance - i->location.distance) < 0.01)
+                if (vehicle != i && vehicle->location.lane == i->location.lane && abs(vehicle->location.distance - i->location.distance) < 0.00287247) // length of a mid-size sedan in miles
+                {
+                    collidedVehicles.first = first;
+                    collidedVehicles.second = second;
                     collision = true;
+                }
+
+                second++;
             }
+
+            first++;
         }
 
         cout << endl;
@@ -43,9 +62,10 @@ int main()
         this_thread::sleep_until(start + chrono::milliseconds(50));
     }
 
-    cout << "Stopping Threads" << endl;
     for (auto& vehicle: vehicles)
         vehicle->stopThread();
+
+    cout << "Collision between vehicle " << collidedVehicles.second << " and vehicle " << collidedVehicles.first << endl;
 
     for (auto& vehicle: vehicles)
         cout << "location: " << vehicle->location.distance << "mi" << endl;
